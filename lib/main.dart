816b882f9d17d182ea0aad559035c15a4502904f
DeadCaramel <pal5686sanjana@gmail.com>
2023-04-07 14:39:29 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class Test extends StatelessWidget {
       ),
       routes: {
         "new_page": (context) => EchoRoute(),
-        "/": (context) => ProgressRoute(),
+        "/": (context) => FlexLayoutTestRoute(),
         "tip2": (context) {
           return TipRoute(
               text: ModalRoute.of(context)!.settings.arguments as String);
@@ -897,5 +897,63 @@ class _ProgressRouteState extends State<ProgressRoute>
             ),
           ),
         ])));
+  }
+}
+
+class FlexLayoutTestRoute extends StatelessWidget {
+  const FlexLayoutTestRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlexLayoutTest'),
+      ),
+      body: Column(
+        children: [
+          Flex(
+            direction: Axis.horizontal,
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 30.0,
+                    color: Colors.red,
+                  )),
+              Expanded(
+                  flex: 3,
+                  child: Container(
+                    height: 30.0,
+                    color: Colors.green,
+                  ))
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: SizedBox(
+              height: 100.0,
+              child: Flex(
+                direction: Axis.vertical,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      height: 30.0,
+                      color: Colors.red,
+                    )),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: 30.0,
+                        color: Colors.green,
+                      ))
+                ],
+                )),)
+        ],
+      ),
+    );
   }
 }
